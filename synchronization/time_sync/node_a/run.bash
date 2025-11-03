1945489e1b0af2b2dc6f0769c30de0e5e6e7193a
devaartana <devaartana11@gmail.com>
2025-11-03 15:41:04 +0700

    update ip

--- synchronization/time_sync/node_a/run.bash
+++ synchronization/time_sync/node_a/run.bash
@@ -2,7 +2,7 @@
 
 python3 peer_node.py \
 		--name A --listen 0.0.0.0 5000 \
-		--peers A@localhost:5000 B@192.168.122.29:5001  C@:5002 D@192.168.122.198:5003 \
-		--logger 192.168.122.52 9999 \
+		--peers A@192.168.122.143:5000 B@192.168.122.143:5001  C@192.168.122.151:5002 D@192.168.122.23:5003 \
+		--logger 192.168.122.49 9999 \
 		--offset-ms 600 \
 		--initiate-broadcast --msg "Hello from A"
